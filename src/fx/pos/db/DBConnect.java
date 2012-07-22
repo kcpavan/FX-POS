aae64e170d0040b62e08818cd82b00a0d7c8149a
pavan <mail2kcpavan@gmail.com>
2012-07-22 22:02:13 +0530

    created initial folder structure

---
+++ src/fx/pos/db/DBConnect.java
@@ -0,0 +1,76 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fx.pos.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author pavankumar
+ */
+public class DBConnect {
+    private static Connection conn;
+
+    private static String url = "jdbc:mysql://localhost:3306/pos";
+
+    private static String user = "root";
+
+    private static String pass = "root";
+
+ 
+
+    public static Connection connect() throws SQLException{
+
+        try{
+
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            
+        }catch(ClassNotFoundException cnfe){
+
+            System.err.println("Error: "+cnfe.getMessage());
+
+        }catch(InstantiationException ie){
+
+            System.err.println("Error: "+ie.getMessage());
+
+        }catch(IllegalAccessException iae){
+
+            System.err.println("Error: "+iae.getMessage());
+
+        }
+
+ 
+
+        conn = DriverManager.getConnection(url,user,pass);
+
+        return conn;
+
+    }
+
+ 
+
+    public static Connection getConnection() throws SQLException{
+
+        if(conn !=null && !conn.isClosed())
+
+            return conn;
+
+        connect();
+
+        return conn;
+
+    }
+    
+    
+    public static void closeConnection() throws SQLException{
+        if(conn !=null && !conn.isClosed())
+            conn.close();
+        
+    }
+
+    
+}
