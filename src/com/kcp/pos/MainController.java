7b487990b30e07890f1a6082bc73513df0ea57a5
Krishna <Krishna@krishna1>
2012-07-29 22:48:01 +0530

    Added form fields for item

--- src/com/kcp/pos/MainController.java
+++ src/com/kcp/pos/MainController.java
@@ -9,7 +9,9 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 /**
  *
@@ -20,6 +22,27 @@ public class MainController implements Initializable {
     @FXML
     private Label label;
     
+    @FXML
+    private TextField itemName;
+     
+    @FXML
+    private TextField itemBarcode;
+    
+    @FXML
+    private TextField itemMrp;
+    
+    @FXML
+    private TextField itemWeight;
+    
+    @FXML
+    private TextField actualPrice;
+     
+    @FXML
+    private TextField sellingPrice;
+    
+    @FXML
+    private CheckBox hasGift;
+     
     @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
